e279000d5d382517fe30c7dc259028f71788fabf
Michael <micke.stefaniak@gmail.com>
2017-06-20 22:17:55 +0200

    test

--- app/src/main/java/com/micste/friendlyweather/DatabaseHelper.java
+++ app/src/main/java/com/micste/friendlyweather/DatabaseHelper.java
@@ -21,6 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
     }
 
     @Override
